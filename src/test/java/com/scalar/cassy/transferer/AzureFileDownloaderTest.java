04259c4b037822ad75162af8bb407a90f8712b77
yume-ind <manter-ben@indetail.co.jp>
2019-12-20 10:31:50 +0900

    updated test class

--- src/test/java/com/scalar/cassy/transferer/AzureFileDownloaderTest.java
+++ src/test/java/com/scalar/cassy/transferer/AzureFileDownloaderTest.java
@@ -1,8 +1,10 @@
 package com.scalar.cassy.transferer;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -14,6 +16,7 @@ import com.azure.storage.blob.models.BlobProperties;
 import com.azure.storage.blob.models.ListBlobsOptions;
 import com.scalar.cassy.config.BackupType;
 import com.scalar.cassy.config.RestoreConfig;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
@@ -30,14 +33,17 @@ public class AzureFileDownloaderTest {
   private static final String ANY_SNAPSHOT_ID = "snapshot_id";
   private static final String ANY_TARGET_IP = "target_ip";
   private static final String ANY_TMP_DATA_DIR = "tmp_data_dir";
-  private static final String ANY_S3_URI = "https://scalar.blob.core.windows.net/container";
-  private static final String MOCKED_BLOB_NAME = "blob_name";
+  private static final String ANY_STOREBASE_URI = "container_name";
   @Mock private BlobContainerAsyncClient containerClient;
   @Mock private BlobAsyncClient blobClient;
   private AzureFileDownloader downloader;
   @Mock private PagedFlux<BlobItem> pagedFlux;
-  @Mock private Mono<BlobProperties> blobPropertiesMono;
-  @Mock private CompletableFuture<BlobProperties> future;
+  @Mock private Mono<BlobProperties> blobPropertiesMono1;
+  @Mock private Mono<BlobProperties> blobPropertiesMono2;
+  @Mock private CompletableFuture<BlobProperties> future1;
+  @Mock private CompletableFuture<BlobProperties> future2;
+  @Mock private BlobProperties blobProperties1;
+  @Mock private BlobProperties blobProperties2;
 
   @Before
   public void setUp() {
@@ -52,15 +58,17 @@ public class AzureFileDownloaderTest {
     props.setProperty(RestoreConfig.RESTORE_TYPE, Integer.toString(type.get()));
     props.setProperty(RestoreConfig.TARGET_IP, ANY_TARGET_IP);
     props.setProperty(RestoreConfig.DATA_DIR, dataDir);
-    props.setProperty(RestoreConfig.STORE_BASE_URI, ANY_S3_URI);
+    props.setProperty(RestoreConfig.STORE_BASE_URI, ANY_STOREBASE_URI);
     props.setProperty(RestoreConfig.KEYSPACE, KEYSPACE_DIR);
     return props;
   }
 
-  private Iterable<BlobItem> getMockedList() {
-    BlobItem blobItem = new BlobItem().setName(MOCKED_BLOB_NAME);
+  private List<BlobItem> getMockedList() {
+    BlobItem blobItem = new BlobItem().setName("blobitem1");
+    BlobItem blobItem2 = new BlobItem().setName("blobitem2");
     List<BlobItem> list = new ArrayList<>();
     list.add(blobItem);
+    list.add(blobItem2);
     return list;
   }
 
@@ -69,17 +77,26 @@ public class AzureFileDownloaderTest {
     // Arrange
     RestoreConfig config =
         new RestoreConfig(getProperties(BackupType.NODE_SNAPSHOT, ANY_TMP_DATA_DIR));
+    List<BlobItem> mockedList = getMockedList();
     when(containerClient.listBlobs(any(ListBlobsOptions.class))).thenReturn(pagedFlux);
-    when(pagedFlux.toIterable()).thenReturn(getMockedList());
+    when(pagedFlux.toIterable()).thenReturn(mockedList);
     when(containerClient.getBlobAsyncClient(anyString())).thenReturn(blobClient);
-    when(blobClient.downloadToFile(anyString())).thenReturn(blobPropertiesMono);
-    when(blobPropertiesMono.toFuture()).thenReturn(future);
-    when(future.get()).thenReturn(mock(BlobProperties.class));
+    when(blobClient.downloadToFile(Paths.get(config.getDataDir(), mockedList.get(0).getName()).toString()))
+        .thenReturn(blobPropertiesMono1);
+    when(blobClient.downloadToFile(Paths.get(config.getDataDir(), mockedList.get(1).getName()).toString()))
+        .thenReturn(blobPropertiesMono2);
+    when(blobPropertiesMono1.toFuture()).thenReturn(future1);
+    when(blobPropertiesMono2.toFuture()).thenReturn(future2);
+    when(future1.get()).thenReturn(blobProperties1);
+    when(future2.get()).thenReturn(blobProperties2);
 
     // Act
     downloader.download(config);
 
     // Assert
-    verify(future).get();
+    verify(future1).get();
+    assertThat(future1.get()).isEqualTo(blobProperties1);
+    verify(future2).get();
+    assertThat(future2.get()).isEqualTo(blobProperties2);
   }
 }
