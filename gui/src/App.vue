fa2eda3694b1a1fc3714f6ea1d5c7caacc1b8e6d
yume-ind <manter-ben@indetail.co.jp>
2020-03-30 17:27:59 +0900

    changed position of navbar button

--- gui/src/App.vue
+++ gui/src/App.vue
@@ -1,14 +1,14 @@
 <template>
     <div>
         <nav class="navbar navbar-collapse navbar-light bg-light">
-            <a class="navbar-brand" href="#">
-<!--                TODO: change the logo for Cassy-->
-                <img class="img-fluid" src="./assets/logo_scalar.svg" height="150" width="150">
-            </a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup"
                     aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
+            <a class="navbar-brand" href="#">
+<!--                TODO: change the logo for Cassy-->
+                <img class="img-fluid" src="./assets/logo_scalar.svg" height="150" width="150">
+            </a>
             <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                 <div class="navbar-nav">
                     <a class="nav-item nav-link active" href="#">View Clusters<span class="sr-only">(current)</span></a>
