7034bfa8727be2979838cc0065da12f09215c564
yume-ind <manter-ben@indetail.co.jp>
2020-03-30 16:18:29 +0900

    created main page for cassy gui, modified entrypoint server for development

---
+++ gui/src/components/Clusters.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="container">
+        <h4 class="mt-3">View Clusters</h4>
+        <table class="table table-borderless table-striped">
+            <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">ID</th>
+                <th scope="col">Name</th>
+                <th scope="col">Created At</th>
+                <th scope="col">Updated At</th>
+            </tr>
+            </thead>
+            <tbody>
+            <tr
+                    v-for="(c, index) in clusters.entries"
+                    :key="index"
+            >
+                <th scope="row">{{index + 1}}</th>
+                <td>{{c.cluster_id}}</td>
+                <td>{{c.cluster_name}}</td>
+                <td>{{ parseInt(c.created_at) | moment("YYYY/M/d, h:mm a")}}</td>
+                <td>{{ parseInt(c.updated_at) | moment("YYYY/M/d h:mm a")}}</td>
+                <td><button type="button" class="btn btn-primary">Manage</button></td>
+            </tr>
+            </tbody>
+        </table>
+    </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      clusters: {
+        type: Object,
+      },
+    },
+  };
+</script>
---
+++ gui/src/main.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import { api } from './api'
+import App from './App.vue'
+import router from './router'
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+Vue.prototype.$api = api;
+Vue.config.productionTip = false
+
+Vue.use(require('vue-moment'));
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
