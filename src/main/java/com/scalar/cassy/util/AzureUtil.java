c37f69086f71062c33679c1cae6af26b8e68d6fa
Hiroyuki Yamada <mogwaing@gmail.com>
2020-05-15 11:24:54 +0900

    support managed identity and service principal for Azure Blob Storage

--- src/main/java/com/scalar/cassy/util/AzureUtil.java
+++ src/main/java/com/scalar/cassy/util/AzureUtil.java
@@ -1,5 +1,6 @@
 package com.scalar.cassy.util;
 
+import com.azure.identity.DefaultAzureCredentialBuilder;
 import com.azure.storage.blob.BlobContainerAsyncClient;
 import com.azure.storage.blob.BlobServiceAsyncClient;
 import com.azure.storage.blob.BlobServiceClientBuilder;
@@ -8,18 +9,22 @@ public class AzureUtil {
   private static final String CONNECTION_STRING = "AZURE_STORAGE_CONNECTION_STRING";
 
   public static BlobContainerAsyncClient getBlobContainerAsyncClient(String storageBaseUri) {
+    BlobServiceClientBuilder builder = new BlobServiceClientBuilder();
+
     String connectionString = System.getenv(CONNECTION_STRING);
     if (connectionString == null) {
-      throw new IllegalArgumentException(
-          "Please set the environment variable '" + CONNECTION_STRING + "'.");
+      // Use service principal or managed identity when a connection string is not given
+      String accountUrl = storageBaseUri.substring(0, storageBaseUri.lastIndexOf('/'));
+      builder.endpoint(accountUrl).credential(new DefaultAzureCredentialBuilder().build());
+    } else {
+      // Use a specified connection string when given
+      builder.connectionString(connectionString);
     }
-    BlobServiceAsyncClient asyncClient =
-        new BlobServiceClientBuilder().connectionString(connectionString).buildAsyncClient();
+    BlobServiceAsyncClient asyncClient = builder.buildAsyncClient();
+
     if (!storageBaseUri.startsWith(asyncClient.getAccountUrl())) {
       throw new IllegalArgumentException(
-          "The container does not belong to the same blob of the "
-              + CONNECTION_STRING
-              + " environment variable");
+          "The given credential can not be used for the specified container.");
     }
     String containerName = storageBaseUri.replace(asyncClient.getAccountUrl() + "/", "");
     return asyncClient.getBlobContainerAsyncClient(containerName);
