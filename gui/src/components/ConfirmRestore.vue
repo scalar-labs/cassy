45440edb7bb2aa7faca69bd9dbfdd3292c497977
yume-ind <manter-ben@indetail.co.jp>
2020-06-08 17:46:44 +0900

    added more info to confirm restore screen

--- gui/src/components/ConfirmRestore.vue
+++ gui/src/components/ConfirmRestore.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="modal fade" tabindex="-1" id="confirmRestore" aria-hidden="true">
-        <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-dialog modal-dialog-centered ">
             <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title">Are you sure you want to restore?</h5>
@@ -9,7 +9,14 @@
                     </button>
                 </div>
                 <div class="modal-body">
-                    <p>Click the button below to confirm you want to restore from this backup.</p>
+                    <div class="row row-cols-2">
+                        <p class="col">Restore Mode:</p>
+                        <p class="col">{{typeName(data.restore_type)}}</p>
+                        <p class="col">Snapshot ID:</p>
+                        <p class="col">{{snapshot_id}}</p>
+                        <p class="col">Target IP(s):</p>
+                        <p class="col">{{target_ips}}</p>
+                    </div>
                 </div>
                 <small v-if="failed" class="form-text text-danger text-center">Error: {{ error }}</small>
                 <div class="modal-footer">
@@ -38,14 +45,25 @@
       return {
         failed: false,
         error: '',
+        target_ips: '',
       };
     },
     mounted() {
       $('#confirmRestore').on('hide.bs.modal', () => {
         this.failed = false;
       });
+      $('#confirmRestore').on('show.bs.modal', () => {
+        if (this.data.target_ip) {
+          this.target_ips = this.data.target_ip;
+        } else {
+          this.target_ips = 'all nodes';
+        }
+      });
     },
     methods: {
+      typeName(t) {
+        return t === 2 ? 'Node' : 'Cluster';
+      },
       returnToRestoreCluster() {
         $('#confirmRestore').modal('hide');
         $('#restoreCluster').modal('show');
@@ -54,7 +72,7 @@
         this.$api.put(`clusters/${this.cluster_id}/data/${this.snapshot_id}`, this.data).then((response) => {
           if (response.status === 200) {
             $('#confirmRestore').modal('hide');
-            this.$router.push({name: 'ViewRestores', params: { snapshot_id: this.snapshot_id}});
+            this.$router.push({name: 'ViewRestores', params: {snapshot_id: this.snapshot_id}});
           }
         }).catch(error => {
           this.error = error.response.data.message;
