bd095bb388c5a238388f6978502d3a7afc9fd9b3
yume-ind <manter-ben@indetail.co.jp>
2020-06-19 11:08:46 +0900

    added form validation for registercluster

--- gui/src/components/RegisterCluster.vue
+++ gui/src/components/RegisterCluster.vue
@@ -10,15 +10,18 @@
                     </button>
                 </div>
                 <div class="modal-body">
-                    <form>
+                    <form class="needs-validation" novalidate>
                         <div class="form-group">
                             <label for="cassandraHostInput">Cassandra host address</label>
                             <input class="form-control" type="text" id="cassandraHostInput" ref="cassandraHostInput"
-                                   placeholder="localhost">
+                                   placeholder="localhost" required>
+                            <div class="invalid-feedback">
+                                Please enter a host address.
+                            </div>
                             <small v-if="failed" class="form-text text-danger">{{ error }}</small>
                         </div>
                         <div class="modal-footer">
-                            <button class="btn btn-primary" data-target="#registerCluster"
+                            <button class="btn btn-primary" data-target="#registerCluster" type="submit"
                                     @click="registerCluster">Register
                             </button>
                         </div>
@@ -56,8 +59,6 @@
             this.error = "Failed to register cluster. Please make sure Cassandra is started."
           } else if (error.response.status === 503) {
             this.error = "Failed to register cluster. Cassy service unavailable."
-          } else {
-            this.error = error.response.data;
           }
         })
         ;
@@ -67,6 +68,22 @@
       $('#registerCluster').on('shown.bs.modal', function() {
         $(this).find('#cassandraHostInput').focus();
       });
+      (function() {
+        'use strict';
+
+        window.addEventListener('load', function() {
+          let forms = document.getElementsByClassName('needs-validation');
+          Array.prototype.filter.call(forms, function(form) {
+            form.addEventListener('submit', function(event) {
+              if (form.checkValidity() === false) {
+                event.preventDefault();
+                event.stopPropagation();
+              }
+              form.classList.add('was-validated');
+            }, false);
+          });
+        }, false);
+      })();
     },
   };
 </script>
