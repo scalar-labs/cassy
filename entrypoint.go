8a24f1c25ae563c8c80e5f37b2b07e4a2d416bdc
Plenty Su <plenty.su@gmail.com>
2020-07-16 15:56:13 +0900

    Rename the module to be github.com/scalar-labs/cassy/entrypoint

--- entrypoint.go
+++ entrypoint.go
@@ -10,7 +10,7 @@ import (
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
 
-	gw "github.com/cassy/entrypoint/src/main/proto"
+	gw "github.com/scalar-labs/cassy/entrypoint/src/main/proto"
 )
 
 var (
