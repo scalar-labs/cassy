a6d7b3d6bef59d9a1befdfb8d3f694308dea4f7d
yume-ind <46988771+yume-ind@users.noreply.github.com>
2020-08-19 11:53:40 +0900

    Revert "Revert "Cassy backup script""

---
+++ src/main/java/com/scalar/cassy/scheduler/CommandModule.java
@@ -0,0 +1,13 @@
+package com.scalar.cassy.scheduler;
+
+import com.google.inject.AbstractModule;
+import com.google.inject.Provides;
+import com.google.inject.Singleton;
+
+public class CommandModule extends AbstractModule {
+  @Provides
+  @Singleton
+  CassyClient provideCassyClient() {
+    return new CassyClient();
+  }
+}
