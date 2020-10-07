0aeef7e01c2b4a7b23a35c75c75e87a6d71aa957
yume-ind <manter-ben@indetail.co.jp>
2020-10-07 14:10:57 +0900

    revised code a bit

--- src/main/java/com/scalar/cassy/scheduler/CassyClient.java
+++ src/main/java/com/scalar/cassy/scheduler/CassyClient.java
@@ -7,10 +7,13 @@ import com.scalar.cassy.rpc.BackupListingResponse;
 import com.scalar.cassy.rpc.BackupRequest;
 import com.scalar.cassy.rpc.BackupResponse;
 import com.scalar.cassy.rpc.CassyGrpc;
+import com.scalar.cassy.rpc.ClusterListingRequest;
+import com.scalar.cassy.rpc.ClusterListingResponse;
 import com.scalar.cassy.rpc.OperationStatus;
 import io.grpc.Channel;
 import io.grpc.ManagedChannelBuilder;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Optional;
 import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutionException;
@@ -66,34 +69,69 @@ public class CassyClient {
 
   public void takeIncrementalBackup(String clusterId, int timeout, Optional<String[]> targetIps)
       throws InterruptedException, TimeoutException, ExecutionException {
-    String snapshotId = "";
-    BackupListingResponse backupListingResponse =
-        blockingStub.listBackups(BackupListingRequest.newBuilder().setClusterId(clusterId).build());
-    for (BackupListingResponse.Entry entry : backupListingResponse.getEntriesList()) {
-      if ((entry.getBackupType() == BackupType.CLUSTER_SNAPSHOT.get()
-              || entry.getBackupType() == BackupType.NODE_SNAPSHOT.get())
-          && entry.getStatusValue() == OperationStatus.COMPLETED_VALUE) {
-        snapshotId = entry.getSnapshotId();
+    // take incremental backups individually by target ips
+
+    // get cluster target ips
+    ClusterListingResponse clusterListingResponse =
+        blockingStub.listClusters(
+            ClusterListingRequest.newBuilder().setClusterId(clusterId).build());
+    for (String targetIp : clusterListingResponse.getEntries(0).getTargetIpsList()) {
+      // for each target_ip, get a backup listing response
+      BackupListingResponse backupListingResponse =
+          blockingStub.listBackups(
+              BackupListingRequest.newBuilder()
+                  .setClusterId(clusterId)
+                  .setTargetIp(targetIp)
+                  .build());
+      List<BackupListingResponse.Entry> entries =
+          backupListingResponse.getEntriesList().stream()
+              .filter(e -> e.getBackupType() != BackupType.NODE_INCREMENTAL.get())
+              .collect(Collectors.toList());
+      // get the most recent snapshot
+      if (entries.get(0).getStatusValue() != OperationStatus.COMPLETED_VALUE) {
+        // if snapshot is not COMPLETED, throw an exception
+        throw new BackupException(
+            "Most recent snapshot is not completed. Unable to take incremental backup.");
       }
+      // if snapshot is COMPLETED, take an incremental backup with target_ip
+      BackupRequest backupRequest =
+          BackupRequest.newBuilder()
+              .setClusterId(clusterId)
+              .setSnapshotId(entries.get(0).getSnapshotId())
+              .setBackupType(BackupType.NODE_INCREMENTAL.get())
+          .build();
+
+      startTask(blockingStub.takeBackup(backupRequest), timeout);
     }
-
-    if (snapshotId.isEmpty()) return; // TODO throw exception?
-
-    BackupRequest backupRequest =
-        BackupRequest.newBuilder()
-            .setClusterId(clusterId)
-            .setSnapshotId(snapshotId)
-            .setBackupType(BackupType.NODE_INCREMENTAL.get())
-            .build();
-
-    if (targetIps.isPresent()) {
-      backupRequest =
-          BackupRequest.newBuilder(backupRequest)
-              .addAllTargetIps(Arrays.stream(targetIps.get()).collect(Collectors.toList()))
-              .build();
-    }
-
-    startTask(blockingStub.takeBackup(backupRequest), timeout);
+//
+//    String snapshotId = "";
+//    BackupListingResponse backupListingResponse =
+//        blockingStub.listBackups(BackupListingRequest.newBuilder().setClusterId(clusterId).build());
+//    for (BackupListingResponse.Entry entry : backupListingResponse.getEntriesList()) {
+//      if ((entry.getBackupType() == BackupType.CLUSTER_SNAPSHOT.get()
+//              || entry.getBackupType() == BackupType.NODE_SNAPSHOT.get())
+//          && entry.getStatusValue() == OperationStatus.COMPLETED_VALUE) {
+//        snapshotId = entry.getSnapshotId();
+//      }
+//    }
+//
+//    if (snapshotId.isEmpty()) return; // TODO throw exception?
+//
+//    BackupRequest backupRequest =
+//        BackupRequest.newBuilder()
+//            .setClusterId(clusterId)
+//            .setSnapshotId(snapshotId)
+//            .setBackupType(BackupType.NODE_INCREMENTAL.get())
+//            .build();
+//
+//    if (targetIps.isPresent()) {
+//      backupRequest =
+//          BackupRequest.newBuilder(backupRequest)
+//              .addAllTargetIps(Arrays.stream(targetIps.get()).collect(Collectors.toList()))
+//              .build();
+//    }
+//
+//    startTask(blockingStub.takeBackup(backupRequest), timeout);
   }
 
   private BackupListingResponse awaitBackupStatusCompletedOrFailed(BackupResponse response)
