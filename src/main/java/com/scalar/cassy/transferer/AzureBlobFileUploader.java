42573c35de744406f73ca4f318e8a44f85d172d4
scalar-boney <boney.jacob@scalar-labs.com>
2021-03-24 16:25:23 +0900

    Review change

--- src/main/java/com/scalar/cassy/transferer/AzureBlobFileUploader.java
+++ src/main/java/com/scalar/cassy/transferer/AzureBlobFileUploader.java
@@ -24,7 +24,7 @@ import reactor.core.publisher.Mono;
 
 public class AzureBlobFileUploader implements FileUploader {
   private static final Logger logger = LoggerFactory.getLogger(AzureBlobFileUploader.class);
-  private static final int asyncFileUploadLimit = 10;
+  private static final int ASYNC_FILE_UPLOAD_LIMIT = 3;
   private final BlobContainerAsyncClient blobContainerClient;
 
   @Inject
@@ -67,15 +67,15 @@ public class AzureBlobFileUploader implements FileUploader {
       } else {
         logger.info(filePath + " has been already uploaded.");
       }
-      if (toBeUploaded.size() % asyncFileUploadLimit == 0) {
-        // Start asynchronously all uploads and wait for all of them to complete
+      if (toBeUploaded.size() % ASYNC_FILE_UPLOAD_LIMIT == 0) {
+        // Start uploading files asynchronously and wait for them to complete
         Mono.when(toBeUploaded).block();
         toBeUploaded.clear();
       }
     }
 
     if (toBeUploaded.size() > 0) {
-      // Start asynchronously all uploads and wait for all of them to complete
+      // Start uploading files asynchronously and wait for them to complete
       Mono.when(toBeUploaded).block();
     }
   }
