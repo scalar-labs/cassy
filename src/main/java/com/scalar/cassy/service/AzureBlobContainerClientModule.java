ad27496706e438d2e7df1cc225a2d11958166719
scalar-boney <boney.jacob@scalar-labs.com>
2021-04-26 11:09:19 +0900

    Blob file async download

--- src/main/java/com/scalar/cassy/service/AzureBlobContainerClientModule.java
+++ src/main/java/com/scalar/cassy/service/AzureBlobContainerClientModule.java
@@ -1,7 +1,6 @@
 package com.scalar.cassy.service;
 
 import com.azure.storage.blob.BlobContainerAsyncClient;
-import com.azure.storage.blob.BlobContainerClient;
 import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
 import com.google.inject.Singleton;
@@ -18,11 +17,5 @@ public abstract class AzureBlobContainerClientModule extends AbstractModule {
   @Singleton
   public BlobContainerAsyncClient provideBlobContainerAsyncClient() {
     return AzureUtil.getBlobContainerAsyncClient(storeBaseUri);
-  }
-
-  @Provides
-  @Singleton
-  public BlobContainerClient provideBlobContainerClient() {
-    return AzureUtil.getBlobContainerClient(storeBaseUri);
   }
 }
--- src/main/java/com/scalar/cassy/transferer/AzureBlobFileDownloader.java
+++ src/main/java/com/scalar/cassy/transferer/AzureBlobFileDownloader.java
@@ -1,8 +1,9 @@
 package com.scalar.cassy.transferer;
 
-import com.azure.storage.blob.BlobContainerClient;
+import com.azure.storage.blob.BlobContainerAsyncClient;
 import com.azure.storage.blob.models.BlobItem;
 import com.azure.storage.blob.models.ListBlobsOptions;
+import com.google.common.collect.Iterators;
 import com.google.inject.Inject;
 import com.scalar.cassy.config.RestoreConfig;
 import com.scalar.cassy.exception.FileTransferException;
@@ -13,15 +14,17 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Iterator;
+import java.util.concurrent.CountDownLatch;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class AzureBlobFileDownloader implements FileDownloader {
   private static final Logger logger = LoggerFactory.getLogger(AzureBlobFileDownloader.class);
-  private BlobContainerClient blobContainerClient;
+  private BlobContainerAsyncClient blobContainerClient;
 
   @Inject
-  public AzureBlobFileDownloader(BlobContainerClient blobContainerClient) {
+  public AzureBlobFileDownloader(BlobContainerAsyncClient blobContainerClient) {
     this.blobContainerClient = blobContainerClient;
   }
 
@@ -30,10 +33,13 @@ public class AzureBlobFileDownloader implements FileDownloader {
     String key = BackupPath.create(config, config.getKeyspace());
 
     logger.info("Downloading " + blobContainerClient.getBlobContainerName() + "/" + key);
-    Iterator<BlobItem> keyspaceBlobs =
-            blobContainerClient.listBlobs(new ListBlobsOptions().setPrefix(key + "/"), null).iterator();
-    while (keyspaceBlobs.hasNext()) {
-      BlobItem blob = keyspaceBlobs.next();
+    Iterable<BlobItem> keyspaceBlobs =
+            blobContainerClient.listBlobs(new ListBlobsOptions().setPrefix(key + "/"), null).toIterable();
+    CountDownLatch countDownLatch = new CountDownLatch(Iterators.size(keyspaceBlobs.iterator()));
+    Iterator<BlobItem> blobList = keyspaceBlobs.iterator();
+
+    while (blobList.hasNext()) {
+      BlobItem blob = blobList.next();
       Path destFile = Paths.get(config.getDataDir(), blob.getName());
 
       try {
@@ -43,15 +49,35 @@ public class AzureBlobFileDownloader implements FileDownloader {
       }
 
       try {
-        try (OutputStream outputStream = new FileOutputStream(destFile.toString())) {
+        OutputStream outputStream = new FileOutputStream(destFile.toString());
           blobContainerClient
-                  .getBlobClient(blob.getName())
-                  .download(outputStream);
-          logger.info("Download file succeeded : " + destFile.toString());
-        }
+                  .getBlobAsyncClient(blob.getName())
+                  .download()
+                  .subscribe(piece->{
+                    try {
+                      outputStream.write(piece.array());
+                    }catch (IOException e){
+                      countDownLatch.countDown();
+                      throw new FileTransferException(e);
+                    }
+                  }, ex ->{
+                    countDownLatch.countDown();
+                    throw new FileTransferException(ex);
+                  }, ()->{
+                    countDownLatch.countDown();
+                    logger.info("Download file succeeded : " + destFile.toString());
+                  });
       } catch (IOException e) {
+        countDownLatch.countDown();
         throw new FileTransferException(e);
       }
+    }
+
+    try {
+      //waiting for asynchronous download completion
+      countDownLatch.await();
+    }catch (InterruptedException ex){
+      throw new FileTransferException(ex);
     }
   }
 
