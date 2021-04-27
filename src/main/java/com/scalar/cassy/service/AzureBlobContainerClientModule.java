12c0e74ecd193a904356e24864ded05c72ff535e
scalar-boney <boney.jacob@scalar-labs.com>
2021-04-27 16:19:13 +0900

    Blob download using sync methods [skip ci]

--- src/main/java/com/scalar/cassy/service/AzureBlobContainerClientModule.java
+++ src/main/java/com/scalar/cassy/service/AzureBlobContainerClientModule.java
@@ -1,21 +1,25 @@
 package com.scalar.cassy.service;
 
 import com.azure.storage.blob.BlobContainerAsyncClient;
+import com.azure.storage.blob.BlobContainerClient;
 import com.google.inject.AbstractModule;
 import com.google.inject.Provides;
 import com.google.inject.Singleton;
 import com.scalar.cassy.util.AzureUtil;
-
 public abstract class AzureBlobContainerClientModule extends AbstractModule {
   private final String storeBaseUri;
-
   public AzureBlobContainerClientModule(String storeBaseUri) {
     this.storeBaseUri = storeBaseUri;
   }
-
   @Provides
   @Singleton
   public BlobContainerAsyncClient provideBlobContainerAsyncClient() {
     return AzureUtil.getBlobContainerAsyncClient(storeBaseUri);
+  }
+
+  @Provides
+  @Singleton
+  public BlobContainerClient provideBlobContainerClient() {
+    return AzureUtil.getBlobContainerClient(storeBaseUri);
   }
 }
